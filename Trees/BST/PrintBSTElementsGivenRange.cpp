c9ba96e73e94067214af56e3fcdbcf1bbc83eb55
Awadesh <36awadesh@gmail.com>
2024-07-13 14:56:49 +0530

    Print BST in Range method 1st

---
+++ Trees/BST/PrintBSTElementsGivenRange.cpp
@@ -0,0 +1,41 @@
+// https://www.geeksforgeeks.org/problems/print-bst-elements-in-given-range/1?page=1&difficulty%5B%5D=0&category%5B%5D=Binary%2520Search%2520Tree&sortBy=submissions
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+};
+// Method 1
+class Solution
+{
+public:
+    vector<int> ans;
+    void inorder(Node *root)
+    {
+        if (!root)
+            return;
+
+        inorder(root->left);
+        ans.push_back(root->data);
+        inorder(root->right);
+    }
+    vector<int> printNearNodes(Node *root, int low, int high)
+    {
+        vector<int> sol;
+        inorder(root);
+        for (auto &it : ans)
+            if (it <= high && it >= low)
+                sol.push_back(it);
+
+        return sol;
+    }
+};
+
+int main()
+{
+    return 0;
+}
