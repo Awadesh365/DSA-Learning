4574f7af14405b4263a5253dfed2ef8fffb3960e
Awadesh <36awadesh@gmail.com>
2024-07-13 15:21:35 +0530

    Print BST elements in given Range method 2

--- Trees/BST/PrintBSTElementsGivenRange.cpp
+++ Trees/BST/PrintBSTElementsGivenRange.cpp
@@ -9,32 +9,65 @@ public:
     Node *left;
     Node *right;
 };
-// Method 1
+//-------------------- Method 1-------------------
+// class Solution
+// {
+// public:
+//     vector<int> ans;
+//     void inorder(Node *root)
+//     {
+//         if (!root)
+//             return;
+
+//         inorder(root->left);
+//         ans.push_back(root->data);
+//         inorder(root->right);
+//     }
+//     vector<int> printNearNodes(Node *root, int low, int high)
+//     {
+//         vector<int> sol;
+//         inorder(root);
+//         for (auto &it : ans)
+//             if (it <= high && it >= low)
+//                 sol.push_back(it);
+
+//         return sol;
+//     }
+// };
+//-----------------------------------------------------
+// Method 2
+int n1, n2;
 class Solution
 {
 public:
-    vector<int> ans;
-    void inorder(Node *root)
+    void find(Node *root, vector<int> &ans)
     {
         if (!root)
             return;
 
-        inorder(root->left);
-        ans.push_back(root->data);
-        inorder(root->right);
+        if ((root->data) > n1 && (root->data) > n2)
+        {
+            find(root->left, ans);
+        }
+        else if ((root->data) < n1 && (root->data) < n2)
+        {
+            find(root->right, ans);
+        }
+        else
+        {
+            find(root->left, ans);
+            ans.push_back(root->data);
+            find(root->right, ans);
+        }
     }
     vector<int> printNearNodes(Node *root, int low, int high)
     {
-        vector<int> sol;
-        inorder(root);
-        for (auto &it : ans)
-            if (it <= high && it >= low)
-                sol.push_back(it);
-
-        return sol;
+        vector<int> ans;
+        n1 = low, n2 = high;
+        find(root, ans);
+        return ans;
     }
 };
-
 int main()
 {
     return 0;
